63b2d1d7c5c778c22ef4abc814288f92d77f68e5
Vinson Kok <vinsonkok@yahoo.com>
2020-01-20 16:49:40 -0600

    Implemented Movement (WASD with rotation facing movement direction) and Camera (360 rotation with right click and zoom with mouse wheel)

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    public float sprintSpeed;
+    public float rotationSmoothing;
+
+    public Transform cameraTransform;
+
+    CharacterController charController;
+    Vector3 moveDirection;
+    float gravity = 1000;
+    float tmpSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        charController = GetComponent<CharacterController>();
+        tmpSpeed = moveSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        Rotation();
+    }
+
+    void Movement()
+    {
+        // get input to move the player while grounded
+        if (charController.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+            // have player move forward in the direction of the camera
+            moveDirection = cameraTransform.TransformDirection(moveDirection);
+            moveDirection *= moveSpeed;
+
+            // sprint input
+            if (Input.GetKey(KeyCode.LeftShift))
+                moveSpeed = sprintSpeed;
+            else
+                moveSpeed = tmpSpeed;
+        }
+
+        // apply gravity in the case that the player isn't grounded 
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        // move the player
+        charController.Move(moveDirection * Time.deltaTime);
+    }
+
+    void Rotation()
+    {
+        // if the player is moving, rotate the player in the direction of movement
+        if (moveDirection.x != 0 || moveDirection.z != 0)
+        {
+            var dir = moveDirection;
+            dir.y = 0;
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), rotationSmoothing);
+        }
+    }
+}
