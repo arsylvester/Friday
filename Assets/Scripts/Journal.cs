ab1c2085f3e30473d6cac8ff46935a7f8b094ec5
Andrew Sylvester <andrewrsylvester@gmail.com>
2020-01-20 19:03:25 -0600

    Journal set up and integrated with dialogue
    This includes basic UI, the ability to save dialogue, and marking of important key dialogue.

---
+++ Assets/Scripts/Journal.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Yarn.Unity;
+
+public class Journal : MonoBehaviour
+{
+    [SerializeField] GameObject itemJournal;
+    [SerializeField] GameObject dialogJournal;
+    [SerializeField] Text dialogText;
+    [SerializeField] GameObject journalTextPrefab;
+
+    private bool dialogSaveable = false;
+    private bool keyDialog = false;
+    private string keyName = "";
+
+    void Start()
+    {
+        itemJournal.SetActive(false);
+        dialogJournal.SetActive(false);
+    }
+
+    void Update()
+    {
+        // Open Journal panels
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            itemJournal.SetActive(!itemJournal.activeSelf);
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            dialogJournal.SetActive(!dialogJournal.activeSelf);
+        }
+
+        //Add dialogue to journal
+        if(dialogSaveable && Input.GetKeyDown(KeyCode.F))
+        {
+            GameObject newJournalText =  Instantiate(journalTextPrefab, dialogJournal.transform);
+            newJournalText.GetComponentInChildren<Text>().text = dialogText.text;
+            if (keyDialog)
+            {
+                newJournalText.GetComponent<DialogueJournalElement>().isKeyDialogue = true;
+                newJournalText.GetComponent<DialogueJournalElement>().keyName = keyName;
+            }
+            CanSaveDialogue(false);
+        }
+    }
+
+    //Toggle the ability to save current dialog to the journal.
+    public void CanSaveDialogue(bool canSave)
+    {
+        dialogSaveable = canSave;
+    }
+
+    //Open both panels. Meant for when dialogue begins.
+    public void OpenJournals()
+    {
+        itemJournal.SetActive(true);
+        dialogJournal.SetActive(true);
+    }
+
+    [YarnCommand("startkey")]
+    public void StartKeyDialogue(string s)
+    {
+        keyDialog = true;
+        keyName = s;
+    }
+
+    [YarnCommand("endkey")]
+    public void EndKeyDialogue()
+    {
+        keyDialog = false;
+        keyName = "";
+    }
+}
---
+++ Assets/Scripts/PlaySound.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+public class PlaySound : MonoBehaviour
+{
+    [System.Serializable]
+    public struct SoundFXInfo
+    {
+        public string name;
+        public AudioClip clip;
+    }
+
+    public SoundFXInfo[] clips;
+
+    /// Create a command to use on a sprite
+    [YarnCommand("playsound")]
+    public void PlayAudio(string clipName)
+    {
+
+        AudioClip c = null;
+        foreach (var info in clips)
+        {
+            if (info.name == clipName)
+            {
+                c = info.clip;
+                break;
+            }
+        }
+        if (c == null)
+        {
+            Debug.LogErrorFormat("Can't find audio clip named {0}!", clipName);
+            return;
+        }
+
+        GetComponent<AudioSource>().PlayOneShot(c);
+    }
+}
