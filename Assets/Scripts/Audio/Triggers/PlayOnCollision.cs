8c37e72f0fe8fb95ab88ca13b50106f5525faac5
Walter Han <wappuchino@gmail.com>
2020-01-23 21:29:03 -0600

    Audio triggers for start and collision

---
+++ Assets/Scripts/Audio/Triggers/PlayOnCollision.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Plays a sound upon entering a collision with any other object.
+/// </summary>
+public class PlayOnCollision : MonoBehaviour
+{
+    /// <summary>
+    /// The audio source containing the sound to be played.
+    /// </summary>
+    public AudioScript AudioSource;
+
+    /// <summary>
+    /// Event handler used to play audio on entering a collision.
+    /// </summary>
+    /// <param name="collision">Unused. Information about the collision.</param>
+    void OnCollisionEnter(Collision collision)
+    {
+        AudioSource.PlaySound(0);
+    }
+}
