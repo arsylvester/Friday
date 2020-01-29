0974c8084bf6b4649483463e4633a672fc1c150d
Walter Han <wappuchino@gmail.com>
2020-01-29 11:09:09 -0600

    Inspection moves instead of copies object

--- Assets/Scripts/Interactions/InspectorAgent.cs
+++ Assets/Scripts/Interactions/InspectorAgent.cs
@@ -9,15 +9,23 @@ using Cinemachine;
 [Serializable] public class PlayerSideInspectEvent : UnityEvent<Inspectable> { }
 public class InspectorAgent : MonoBehaviour
 {
+
+    public float InspectionDistance;
+    public float FocusTime;
     public Text FlavorText;
     public string InteractKey;
-    public CinemachineVirtualCameraBase InspectorFreeLook;
     public Camera InspectorCamera;
 
     public PlayerSideInspectEvent OnFocused;
     public PlayerSideInspectEvent OnUnfocused;
 
+    private IEnumerator focusCoroutine;
     private Inspectable target;
+    private Quaternion originalRotation;
+    private Vector3 originalPosition;
+
+    private Dictionary<Inspectable, IEnumerator> unfocusCoroutines = new Dictionary<Inspectable, IEnumerator>();
+
     private Camera mainCamera;
     void Start()
     {
@@ -36,31 +44,99 @@ public class InspectorAgent : MonoBehaviour
 
                 if (!hit || rayInfo.collider.gameObject != target.gameObject)
                 {
-                    Debug.Log("A");
                     Release();
                 }
             }
+
+            
         }
     }
+
+    public static float Sirp(float y0, float y1, float t)
+    {
+        float dy = y1 - y0;
+        return (Mathf.Sin((Mathf.Clamp01(t) + 1.5F) * Mathf.PI) + 1) * dy / 2 + y0;
+    }
+
     public void Inspect(Interactable interactable)
     {
         Inspectable inspectable = interactable.GetComponent<Inspectable>();
         if(inspectable != null)
         {
             target = inspectable;
+
+            if(unfocusCoroutines.ContainsKey(inspectable) && unfocusCoroutines[inspectable] != null)
+            {
+                StopCoroutine(unfocusCoroutines[inspectable]);
+            }
+
+            Transform originalTransform = target.transform;
+            originalPosition = originalTransform.position;
+            originalRotation = originalTransform.rotation;
+
             FlavorText.text = target.InspectorText;
-            InspectorFreeLook.LookAt = target.transform;
-            InspectorFreeLook.Follow = target.transform;
-            target.gameObject.layer = LayerMask.NameToLayer("Inspector");
             OnFocused.Invoke(target);
+            focusCoroutine = FocusObject(target.gameObject);
+            StartCoroutine(focusCoroutine);
         }
+    }
+
+    public IEnumerator FocusObject(GameObject target)
+    {
+        float t = 0;
+        Vector3 targetPosition;
+        Vector3 startPosition = target.transform.position;
+        target.layer = LayerMask.NameToLayer("Inspector");
+
+        do
+        {
+            t += Time.deltaTime;
+            targetPosition = transform.position + transform.forward * InspectionDistance;
+            target.transform.position = Vector3.Lerp(startPosition, targetPosition, Sirp(0, 1, t));
+            yield return null;
+
+        } while (!Mathf.Approximately((targetPosition - target.transform.position).sqrMagnitude, 0.0F));
+
+        Debug.Log("Focused");
+    }
+
+    public IEnumerator UnfocusObject(GameObject target, Quaternion originalRotation, Vector3 originalPosition)
+    {
+        float t = 0;
+        Vector3 originalEuler = originalRotation.eulerAngles;
+        Vector3 focusAngularVelocity = Vector3.zero;
+        Vector3 startingPosition = target.transform.position;
+        Vector3 startingEuler = target.transform.rotation.eulerAngles;
+
+        do
+        {
+            t += Time.deltaTime;
+            target.transform.position = Vector3.Lerp(startingPosition, originalPosition, Sirp(0, 1, t));
+            Vector3 eulerRotation = target.transform.rotation.eulerAngles;
+            eulerRotation.x = Sirp(startingEuler.x, originalEuler.x, t);
+            eulerRotation.y = Sirp(startingEuler.y, originalEuler.y, t);
+            eulerRotation.z = Sirp(startingEuler.z, originalEuler.z, t);
+            yield return null;
+        } while (!Mathf.Approximately((originalPosition - target.transform.position).sqrMagnitude, 0.0F) ||
+            !Mathf.Approximately(Quaternion.Angle(target.transform.rotation, originalRotation), 0.0F));
+
+        target.layer = LayerMask.NameToLayer("Default");
+        Debug.Log("Unfocused");
     }
 
     public void Release()
     {
+        if(focusCoroutine != null)
+        {
+            StopCoroutine(focusCoroutine);
+        }
+
         OnUnfocused.Invoke(target);
-        target.gameObject.layer = LayerMask.NameToLayer("Default");
         FlavorText.text = "";
+
+        IEnumerator unfocusCoroutine = UnfocusObject(target.gameObject, originalRotation, originalPosition);
+        unfocusCoroutines[target] = unfocusCoroutine;
+        StartCoroutine(unfocusCoroutine);
         target = null;
     }
 }
