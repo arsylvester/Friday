61f2fd84cd0c24d99bea541b151a4d31414c0ff2
Walter Han <wappuchino@gmail.com>
2020-02-02 21:01:24 -0600

    Interactable Items with Secrets and Proper Nametags

---
+++ Assets/Scripts/Interactions/ItemAgent.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemAgent : MonoBehaviour
+{
+    public string RecordKey;
+    public Camera CaptureCamera;
+    public Text FlavorText;
+    public Image DebugOutput;
+
+    private Item item;
+    public void FocusItem(Inspectable item)
+    {
+        this.item = item as Item;
+    }
+
+    public void UnfocusItem()
+    {
+        this.item = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(item != null)
+        {
+            FlavorText.text = item.FlavorText;
+
+            if(Input.GetButtonDown(RecordKey))
+            {
+                SecretItem secret = null;
+                foreach (SecretItem s in item.GetComponentsInChildren<SecretItem>())
+                {
+                    if(!Physics.Linecast(transform.position, s.transform.position, LayerMask.GetMask("Inspector")))
+                    {
+                        secret = s;
+                    }
+                }
+
+                Debug.Log("Record");
+                CaptureCamera.Render();
+                RenderTexture rt = CaptureCamera.targetTexture;
+                Texture2D td = new Texture2D(rt.width, rt.height, TextureFormat.RGBA32, false, true);
+                td.Apply(false);
+                Graphics.CopyTexture(rt, td);
+                Sprite capture = Sprite.Create(td, new Rect(0, 0, rt.width, rt.height), new Vector2(rt.width / 2.0F, rt.height / 2.0F));
+                DebugOutput.sprite = capture;
+
+                if(secret != null)
+                {
+                    Debug.Log("Secret");
+                    //TODO Pass secret to journal
+                }
+                else
+                {
+                    //TODO Pass item to journal
+                }
+            }
+        }
+        else
+        {
+            FlavorText.text = "";
+        }
+    }
+}
---
+++ Assets/Scripts/Interactions/SecretItem.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecretItem : MonoBehaviour
+{
+    public string EntryName;
+    public string Description;
+    public string FlavorText;
+    public string Keyname;
+}
