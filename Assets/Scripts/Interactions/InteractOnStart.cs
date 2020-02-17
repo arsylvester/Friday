3ca137b8ff65f1c9c21654fdfd6e911e8d684e3a
Andrew Sylvester <andrewrsylvester@gmail.com>
2020-02-16 18:51:21 -0600

    Start dialogue at beginning of scene and some UI fixes

---
+++ Assets/Scripts/Interactions/InteractOnStart.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractOnStart : MonoBehaviour
+{
+    [SerializeField] NPCAgent agent; 
+    [SerializeField] Interactable NPC;
+    [SerializeField] float waitTime = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(StartDialogue());
+    }
+
+    IEnumerator StartDialogue()
+    {
+        yield return new WaitForSeconds(waitTime);
+        agent.InitiateDialogue(NPC);
+    }
+
+}
