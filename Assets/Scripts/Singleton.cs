74524bc608396a35b2e403264e2806f687f417a3
Vinson Kok <vinsonkok@yahoo.com>
2020-02-16 22:28:54 -0600

    Destroy Immediate

--- Assets/Scripts/Singleton.cs
+++ Assets/Scripts/Singleton.cs
@@ -12,6 +12,6 @@ public class Singleton : MonoBehaviour
         if (instance == null)
             instance = this;
         else if (instance != this)
-            Destroy(gameObject);
+            DestroyImmediate(gameObject);
     }
 }
