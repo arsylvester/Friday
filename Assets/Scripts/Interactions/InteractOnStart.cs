ad7bcfe87ba14d80fd1bc4bc03a0a3bd2c859282
Andrew Sylvester <andrewrsylvester@gmail.com>
2020-02-17 23:05:57 -0600

    Several minor bug fixes and tweaks

--- Assets/Scripts/Interactions/InteractOnStart.cs
+++ Assets/Scripts/Interactions/InteractOnStart.cs
@@ -8,16 +8,20 @@ public class InteractOnStart : MonoBehaviour
     [SerializeField] Interactable NPC;
     [SerializeField] float waitTime = 1f;
 
+    private static bool isFirstTime = true; //Probably want to remove in the future.
+
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(StartDialogue());
+        if(isFirstTime)
+            StartCoroutine(StartDialogue());
     }
 
     IEnumerator StartDialogue()
     {
         yield return new WaitForSeconds(waitTime);
         agent.Interact(NPC);
+        isFirstTime = false;
     }
 
 }
