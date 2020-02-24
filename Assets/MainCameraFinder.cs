abc93b8273f8cdfb162045d9f1e5cac89b2c4237
Walter Han <wappuchino@gmail.com>
2020-02-23 22:39:08 -0600

    Dialogue draws in camera space instead of screen space

---
+++ Assets/MainCameraFinder.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainCameraFinder : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneManager.sceneLoaded += ReloadCamera;
+        ReloadCamera(SceneManager.GetActiveScene(), LoadSceneMode.Single);
+    }
+
+    public void ReloadCamera(Scene scene, LoadSceneMode mode)
+    {
+        GetComponent<Canvas>().worldCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
+    }
+}
