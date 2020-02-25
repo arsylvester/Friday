b4fcc4c14fe1150d6959205c7ee192ef8e840dc6
Walter Han <wappuchino@gmail.com>
2020-02-25 15:11:08 -0600

    Tutorial state added to moon

---
+++ Assets/Scripts/Tutorial/TutorialState.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class TutorialState : MonoBehaviour
+{
+    private static string[] STATES =
+    {
+        "tutorialStart",
+        "mainMenu1",
+        "leaveConvo",
+        "inspectorTutorial",
+        "reenterConvo",
+        "followUpQuestions",
+        "recordDialogue",
+        "mainMenu2",
+        "interrogate",
+        "comboAttempt1",
+        "failCombo",
+        "stocking",
+        "done"
+    };
+
+    private static string state = STATES[0];
+
+    public static void Next()
+    {
+        state = STATES[STATES.ToList().FindIndex(x => x == state) + 1];
+    }
+
+    public static string Current
+    {
+        get { return state; }
+    }
+}
