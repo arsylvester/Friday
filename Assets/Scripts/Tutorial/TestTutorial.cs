22c88336edc4b47dbe731a93c6aa4cee80bfc2d6
Andrew Sylvester <andrewrsylvester@gmail.com>
2020-02-26 17:56:09 -0600

    Tutorial Prompt script Set up

---
+++ Assets/Scripts/Tutorial/TestTutorial.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestTutorial : MonoBehaviour
+{
+    [SerializeField] GameObject testPrompts;
+    [SerializeField] TutorialPrompt tutorialPrompt;
+
+    public void StartTest()
+    {
+        tutorialPrompt.StartPrompt(gameObject, testPrompts);
+        GetComponent<Button>().onClick.AddListener(EndTest);
+        GetComponent<Button>().onClick.RemoveListener(StartTest);
+    }
+
+    public void EndTest()
+    {
+        tutorialPrompt.EndPrompt();
+    }
+}
---
+++ Assets/Scripts/Tutorial/TutorialPrompt.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialPrompt : MonoBehaviour
+{
+    [SerializeField] GameObject fadePanel;
+    [SerializeField] float fadeDepth = 10;
+
+    private GameObject highlighted;
+    //private float orgDepth;
+    private GameObject currentPrompts;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Vector3 temp = fadePanel.transform.position;
+        //temp.z = fadeDepth;
+        //fadePanel.transform.position = temp;
+    }
+
+    public void StartPrompt(GameObject highlightedObject, GameObject prompts)
+    {
+        highlighted = highlightedObject;
+        currentPrompts = prompts;
+        //orgDepth = highlightedObject.position.z;
+
+        //Vector3 temp = highlightedObject.position;
+        //temp.z = fadeDepth + 5;
+        //highlightedObject.position = temp;
+
+        prompts.SetActive(true);
+        fadePanel.SetActive(true);
+
+        Instantiate(highlightedObject, transform, true);
+    }
+
+    public void EndPrompt()
+    {
+        //Vector3 temp = highlighted.position;
+        //temp.z = orgDepth;
+        //highlighted.position = temp;
+
+        currentPrompts.SetActive(false);
+        fadePanel.SetActive(false);
+
+        Destroy(highlighted);
+
+        TutorialState.Next();
+    }
+}
