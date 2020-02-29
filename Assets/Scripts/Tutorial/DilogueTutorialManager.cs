0965333ade3db728a096b8d786308da7500db088
Andrew Sylvester <andrewrsylvester@gmail.com>
2020-02-29 12:52:18 -0600

    Some more tutorial steps

--- Assets/Scripts/Tutorial/DilogueTutorialManager.cs
+++ Assets/Scripts/Tutorial/DilogueTutorialManager.cs
@@ -17,6 +17,14 @@ public class DilogueTutorialManager : MonoBehaviour
     [SerializeField] Button continueButton;
     
     [SerializeField] GameObject MM1Prompt;
+    [SerializeField] GameObject leavePrompt;
+    [SerializeField] GameObject FUQPrompt;
+    [SerializeField] GameObject recDiaPrompt;
+    [SerializeField] GameObject MM2Prompt;
+    [SerializeField] GameObject interrogatePrompt;
+    [SerializeField] GameObject comboAttemptPrompt;
+    [SerializeField] GameObject failComboPrompt;
+    [SerializeField] GameObject stockingPrompt;
     [SerializeField] TutorialPrompt tutorialPrompt;
 
     // Start is called before the first frame update
@@ -30,9 +38,9 @@ public class DilogueTutorialManager : MonoBehaviour
     {
         
     }
-    IEnumerator waitThenPrompt(GameObject highlight, GameObject currrPrompt)
+    IEnumerator waitThenPrompt(GameObject highlight, GameObject currrPrompt, float waitTime)
     {
-        yield return new WaitForSeconds(.5f);
+        yield return new WaitForSeconds(waitTime);
         tutorialPrompt.StartPrompt(highlight, currrPrompt, true);
     }
 
@@ -47,7 +55,28 @@ public class DilogueTutorialManager : MonoBehaviour
                 break;
             case "mainMenu1":
                 //convoAgainButton.gameObject.SetActive(true);
-                StartCoroutine(waitThenPrompt(convoAgainButton.gameObject, MM1Prompt));
+                StartCoroutine(waitThenPrompt(convoAgainButton.gameObject, MM1Prompt, .05f));
+                break;
+            case "leaveConvo":
+                leaveButton.gameObject.SetActive(true);
+                StartCoroutine(waitThenPrompt(leaveButton.gameObject, leavePrompt, .9f));
+                break;
+            case "followUpQuestions":
+                StartCoroutine(waitThenPrompt(followUpQButton.gameObject, FUQPrompt, .05f));
+                break;
+            case "recordDialogue":
+                break;
+            case "mainMenu2":
+                break;
+            case "interrogate":
+                break;
+            case "comboAttempt1":
+                break;
+            case "failCombo":
+                break;
+            case "stocking":
+                break;
+            case "done":
                 break;
             default:
                 break;
@@ -62,9 +91,38 @@ public class DilogueTutorialManager : MonoBehaviour
             case "mainMenu1":
                 convoAgainButton.onClick.SetPersistentListenerState(2, UnityEventCallState.Off);
                 break;
+             case "leaveConvo":
+                leaveButton.onClick.SetPersistentListenerState(4, UnityEventCallState.Off);
+                break;
+            case "followUpQuestions":
+                followUpQButton.onClick.SetPersistentListenerState(2, UnityEventCallState.Off);
+                break;
+            case "recordDialogue":
+                break;
+            case "mainMenu2":
+                break;
+            case "interrogate":
+                break;
+            case "comboAttempt1":
+                break;
+            case "failCombo":
+                break;
+            case "stocking":
+                break;
+            case "done":
+                break;
             default:
                 break;
         }
         tutorialPrompt.EndPrompt();
+    }
+
+    //For Testing
+    [YarnCommand("nextTutState")]
+    public void nextTutState()
+    {
+        TutorialState.Next();
+        TutorialState.Next();
+        print(TutorialState.Current);
     }
 }
