0960a899ce9b985808731930fcf62f7e0f7b0424
Walter Han <wappuchino@gmail.com>
2020-05-31 17:20:50 -0500

    Fixed single still scene prompt to disallow player actions

--- Assets/Scripts/UI/SingleStillPrompt.cs
+++ Assets/Scripts/UI/SingleStillPrompt.cs
@@ -6,28 +6,33 @@ using UnityEngine.UI;
 public class SingleStillPrompt : MonoBehaviour
 {
     private Text text;
+    public GameObject MapUI;
     // Start is called before the first frame update
+
+    private PlayerInteractionAgent agent;
+    private PlayerMovement movement;
+    private InputFreeLookCam cam;
+
     void Start()
     {
         text = GetComponent<Text>();
         text.enabled = true;
         text.text += "\n\n(Left click to continue)";
 
-        FindObjectOfType<PlayerInteractionAgent>().enabled = false;
-        FindObjectOfType<PlayerMovement>().StopMovement();
-        FindObjectOfType<InputFreeLookCam>().FreezeCamera();
+        agent = FindObjectOfType<PlayerInteractionAgent>();
+        movement = FindObjectOfType<PlayerMovement>();
+        cam = FindObjectOfType<InputFreeLookCam>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        agent.enabled = false;
+        movement.StopMovement();
+        cam.FreezeCamera();
         if(Input.GetKeyDown(KeyCode.Mouse0))
         {
-            text.enabled = false;
-            FindObjectOfType<PlayerInteractionAgent>().enabled = true;
-            FindObjectOfType<InputFreeLookCam>().UnfreezeCamera();
-            FindObjectOfType<PlayerMovement>().ResumeMovement();
-            this.enabled = false;
+            MapUI.SetActive(true);
         }
     }
 }
