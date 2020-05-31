9041616f4f4ece69db7bbbc324f6aa15730594ea
Walter Han <wappuchino@gmail.com>
2020-05-31 17:38:59 -0500

    Setup for modifiable item names upon completing a deduction

---
+++ Assets/Scripts/Interactions/ChangeTextOnFlag.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeTextOnFlag : MonoBehaviour
+{
+    private Item item;
+    private Journal journal;
+    public string FlagName = "";
+    public string PostFlagText = "";
+    // Start is called before the first frame update
+    void Start()
+    {
+        journal = FindObjectOfType<Journal>();
+        item = GetComponent<Item>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(journal.HasCompletedObjective(FlagName))
+        {
+            item.name = PostFlagText;
+        }
+    }
+}
