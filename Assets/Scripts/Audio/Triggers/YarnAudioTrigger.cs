9bc86ce08bad3080fe0cd959b6f435c376be8859
Andrew Sylvester <andrewrsylvester@gmail.com>
2020-06-26 13:13:23 -0500

    Added USB and Safe sounds

---
+++ Assets/Scripts/Audio/Triggers/YarnAudioTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+public class YarnAudioTrigger : MonoBehaviour
+{
+    private AudioScript audioScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioScript = GetComponent<AudioScript>();
+    }
+
+    [YarnCommand("PlaySound")]
+    public void PlaySound()
+    {
+        audioScript.PlaySound(0);
+    }
+}
